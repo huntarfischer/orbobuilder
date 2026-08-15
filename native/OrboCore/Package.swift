a2c743c8930d7c94dc09bc0dcbca5633f677807b
huntarfischer <huntarfischer@gmail.com>
2026-08-15 16:18:57 -0500

    Phase 0: establish OrboCore Swift package

---
+++ native/OrboCore/Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version: 5.9
+
+import PackageDescription
+
+let package = Package(
+    name: "OrboCore",
+    platforms: [
+        .iOS(.v17),
+        .macOS(.v14)
+    ],
+    products: [
+        .library(
+            name: "OrboCore",
+            targets: ["OrboCore"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "OrboCore"
+        ),
+        .testTarget(
+            name: "OrboCoreTests",
+            dependencies: ["OrboCore"]
+        )
+    ]
+)
