29eb6b681bf0562f71c51195b8a8c2404713ecd2
huntarfischer <huntarfischer@gmail.com>
2026-08-15 21:27:08 -0500

    Register native test fixture resources

--- native/OrboCore/Package.swift
+++ native/OrboCore/Package.swift
@@ -20,7 +20,10 @@ let package = Package(
         ),
         .testTarget(
             name: "OrboCoreTests",
-            dependencies: ["OrboCore"]
+            dependencies: ["OrboCore"],
+            resources: [
+                .copy("Fixtures")
+            ]
         )
     ]
 )
