a5c0faf5a2a00f1c29f6e310c527e8428fc7412e
huntarfischer <huntarfischer@gmail.com>
2026-08-20 21:06:01 -0500

    Harden Clotho thread ownership and preserve arcsecond precision

--- native/OrboCore/Sources/OrboCore/Connectome/Clotho.swift
+++ native/OrboCore/Sources/OrboCore/Connectome/Clotho.swift
@@ -1,9 +1,9 @@
-public struct ClothoNatalFact: Hashable, Sendable {
+public struct ClothoThread: Hashable, Sendable {
     public let gene: AstroDNAGene
     public let exactState: RingFineState
     public let degreeAddress: DegreeAddress
 
-    public init(
+    fileprivate init(
         gene: AstroDNAGene,
         exactState: RingFineState,
         degreeAddress: DegreeAddress
@@ -15,29 +15,29 @@ public struct ClothoNatalFact: Hashable, Sendable {
 }
 
 public struct ClothoSourcePacket: Hashable, Sendable {
-    public let natalFacts: [ClothoNatalFact]
+    public let threads: [ClothoThread]
 
-    public init(natalFacts: [ClothoNatalFact]) {
-        self.natalFacts = natalFacts
+    fileprivate init(threads: [ClothoThread]) {
+        self.threads = threads
     }
 }
 
 public enum Clotho {
     public static func gather(from natalAstroDNA: AstroDNA) -> ClothoSourcePacket {
-        let natalFacts = AstroDNAGene.canonicalOrder.map { gene in
+        let threads = AstroDNAGene.canonicalOrder.map { gene in
             let exactState = natalAstroDNA[gene]
             let degree = exactState.coarseState.degree
             guard let degreeAddress = DegreeAddress(rawValue: degree) else {
                 preconditionFailure("Clotho received an invalid whole-degree Ring address.")
             }
 
-            return ClothoNatalFact(
+            return ClothoThread(
                 gene: gene,
                 exactState: exactState,
                 degreeAddress: degreeAddress
             )
         }
 
-        return ClothoSourcePacket(natalFacts: natalFacts)
+        return ClothoSourcePacket(threads: threads)
     }
 }
