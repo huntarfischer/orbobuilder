26af26a4289f35890cea06a7dec9221ef486ba86
huntarfischer <huntarfischer@gmail.com>
2026-08-21 01:18:31 -0500

    Stage 3: have Clotho emit Moirai recipe with threads

--- native/OrboCore/Sources/OrboCore/Connectome/Clotho.swift
+++ native/OrboCore/Sources/OrboCore/Connectome/Clotho.swift
@@ -22,22 +22,73 @@ public struct ClothoSourcePacket: Hashable, Sendable {
     }
 }
 
+public struct MoiraiRecipeEntry: Hashable, Sendable {
+    public let gene: AstroDNAGene
+    public let exactState: RingFineState
+    public let degreeAddress: DegreeAddress
+
+    fileprivate init(
+        gene: AstroDNAGene,
+        exactState: RingFineState,
+        degreeAddress: DegreeAddress
+    ) {
+        self.gene = gene
+        self.exactState = exactState
+        self.degreeAddress = degreeAddress
+    }
+}
+
+public struct MoiraiRecipe: Hashable, Sendable {
+    public let entries: [MoiraiRecipeEntry]
+
+    fileprivate init(entries: [MoiraiRecipeEntry]) {
+        self.entries = entries
+    }
+}
+
+public struct ClothoOutput: Hashable, Sendable {
+    public let packet: ClothoSourcePacket
+    public let recipe: MoiraiRecipe
+
+    fileprivate init(packet: ClothoSourcePacket, recipe: MoiraiRecipe) {
+        self.packet = packet
+        self.recipe = recipe
+    }
+}
+
 public enum Clotho {
-    public static func gather(from natalAstroDNA: AstroDNA) -> ClothoSourcePacket {
-        let threads = AstroDNAGene.canonicalOrder.map { gene in
+    public static func gather(from natalAstroDNA: AstroDNA) -> ClothoOutput {
+        var threads: [ClothoThread] = []
+        var recipeEntries: [MoiraiRecipeEntry] = []
+        threads.reserveCapacity(AstroDNA.geneCount)
+        recipeEntries.reserveCapacity(AstroDNA.geneCount)
+
+        for gene in AstroDNAGene.canonicalOrder {
             let exactState = natalAstroDNA[gene]
             let degree = exactState.coarseState.degree
             guard let degreeAddress = DegreeAddress(rawValue: degree) else {
                 preconditionFailure("Clotho received an invalid whole-degree Ring address.")
             }
 
-            return ClothoThread(
-                gene: gene,
-                exactState: exactState,
-                degreeAddress: degreeAddress
+            threads.append(
+                ClothoThread(
+                    gene: gene,
+                    exactState: exactState,
+                    degreeAddress: degreeAddress
+                )
+            )
+            recipeEntries.append(
+                MoiraiRecipeEntry(
+                    gene: gene,
+                    exactState: exactState,
+                    degreeAddress: degreeAddress
+                )
             )
         }
 
-        return ClothoSourcePacket(threads: threads)
+        return ClothoOutput(
+            packet: ClothoSourcePacket(threads: threads),
+            recipe: MoiraiRecipe(entries: recipeEntries)
+        )
     }
 }
