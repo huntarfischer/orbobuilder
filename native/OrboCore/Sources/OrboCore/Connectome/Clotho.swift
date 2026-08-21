3ba1526b108f04a54773dc7f46c9632d1883fd44
huntarfischer <huntarfischer@gmail.com>
2026-08-20 20:57:26 -0500

    Correct Clotho to gather degree-addressed natal facts

--- native/OrboCore/Sources/OrboCore/Connectome/Clotho.swift
+++ native/OrboCore/Sources/OrboCore/Connectome/Clotho.swift
@@ -1,13 +1,43 @@
-public struct ClothoSourcePacket: Codable, Hashable, Sendable {
-    public let risingSign: Sign
+public struct ClothoNatalFact: Hashable, Sendable {
+    public let gene: AstroDNAGene
+    public let exactState: RingFineState
+    public let degreeAddress: DegreeAddress
 
-    public init(risingSign: Sign) {
-        self.risingSign = risingSign
+    public init(
+        gene: AstroDNAGene,
+        exactState: RingFineState,
+        degreeAddress: DegreeAddress
+    ) {
+        self.gene = gene
+        self.exactState = exactState
+        self.degreeAddress = degreeAddress
+    }
+}
+
+public struct ClothoSourcePacket: Hashable, Sendable {
+    public let natalFacts: [ClothoNatalFact]
+
+    public init(natalFacts: [ClothoNatalFact]) {
+        self.natalFacts = natalFacts
     }
 }
 
 public enum Clotho {
     public static func gather(from natalAstroDNA: AstroDNA) -> ClothoSourcePacket {
-        ClothoSourcePacket(risingSign: natalAstroDNA.sign(of: .ascendant))
+        let natalFacts = AstroDNAGene.canonicalOrder.map { gene in
+            let exactState = natalAstroDNA[gene]
+            let degree = exactState.coarseState.degree
+            guard let degreeAddress = DegreeAddress(rawValue: degree) else {
+                preconditionFailure("Clotho received an invalid whole-degree Ring address.")
+            }
+
+            return ClothoNatalFact(
+                gene: gene,
+                exactState: exactState,
+                degreeAddress: degreeAddress
+            )
+        }
+
+        return ClothoSourcePacket(natalFacts: natalFacts)
     }
 }
