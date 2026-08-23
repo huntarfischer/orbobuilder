22dea3554c6ff9aad872b8a0f6915a5e5fa2b3de
huntarfischer <huntarfischer@gmail.com>
2026-08-23 15:29:11 -0500

    Build Atlas v1 authority and Topos

---
+++ native/OrboCore/Sources/OrboCore/OrboSystem/Atlas/Atlas.swift
@@ -0,0 +1,53 @@
+import Foundation
+
+public struct AtlasProvenance: Hashable, Codable, Sendable {
+    public let version: String
+    public let sourceDescription: String
+
+    fileprivate init(version: String, sourceDescription: String) {
+        self.version = version
+        self.sourceDescription = sourceDescription
+    }
+}
+
+public struct Topos: Hashable, Codable, Sendable {
+    public let place: Place
+    public let provenance: AtlasProvenance
+
+    fileprivate init(place: Place, provenance: AtlasProvenance) {
+        self.place = place
+        self.provenance = provenance
+    }
+}
+
+public enum AtlasResolution: Equatable, Sendable {
+    case found(Topos)
+    case ambiguous([Topos])
+    case notFound
+}
+
+public struct Atlas: Sendable {
+    public init() {}
+
+    public func resolve(_ query: String) -> AtlasResolution {
+        switch GeoplacementAtlas.resolve(query) {
+        case let .found(place):
+            return .found(topos(for: place))
+        case let .ambiguous(places):
+            return .ambiguous(places.map(topos(for:)))
+        case .notFound:
+            return .notFound
+        }
+    }
+
+    private var provenance: AtlasProvenance {
+        AtlasProvenance(
+            version: GeoplacementAtlas.version,
+            sourceDescription: GeoplacementAtlas.sourceDescription
+        )
+    }
+
+    private func topos(for place: Place) -> Topos {
+        Topos(place: place, provenance: provenance)
+    }
+}
