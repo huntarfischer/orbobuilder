16a0d4c48c57c0d801472f10998ae19b57fa5e75
huntarfischer <huntarfischer@gmail.com>
2026-08-23 16:46:43 -0500

    Name Hermes service routing Messenger

---
+++ native/OrboCore/Sources/OrboCore/OrboSystem/Hermes/HermesMessengerRouteRegistry.swift
@@ -0,0 +1,57 @@
+public struct HermesMessengerRouteContract: Hashable, Codable, Sendable {
+    public let serviceDestination: HermesAddress
+    public let acceptedParcelKind: HermesParcelKind
+    public let expectedReturnKind: HermesParcelKind
+
+    public init(
+        serviceDestination: HermesAddress,
+        acceptedParcelKind: HermesParcelKind,
+        expectedReturnKind: HermesParcelKind
+    ) {
+        self.serviceDestination = serviceDestination
+        self.acceptedParcelKind = acceptedParcelKind
+        self.expectedReturnKind = expectedReturnKind
+    }
+}
+
+public struct HermesMessengerRouteRegistry: Sendable {
+    private let contracts: [HermesMessengerRouteContract]
+    private let finalAcceptances: [HermesAddress: Set<HermesParcelKind>]
+
+    public init() {
+        let moirai = HermesAddress(rawValue: "orbo.moirai")!
+        let hestia = HermesAddress(rawValue: "orbo.hestia")!
+        let natalCommission = HermesParcelKind(rawValue: "orbo.natal-commission.v1")!
+        let moiraiPackage = HermesParcelKind(rawValue: "orbo.moirai-package.v1")!
+
+        self.contracts = [
+            HermesMessengerRouteContract(
+                serviceDestination: moirai,
+                acceptedParcelKind: natalCommission,
+                expectedReturnKind: moiraiPackage
+            ),
+        ]
+        self.finalAcceptances = [
+            hestia: [moiraiPackage],
+        ]
+    }
+
+    public func contract(
+        for serviceDestination: HermesAddress,
+        accepting parcelKind: HermesParcelKind,
+        returning expectedReturnKind: HermesParcelKind
+    ) -> HermesMessengerRouteContract? {
+        contracts.first {
+            $0.serviceDestination == serviceDestination
+                && $0.acceptedParcelKind == parcelKind
+                && $0.expectedReturnKind == expectedReturnKind
+        }
+    }
+
+    public func finalAddressee(
+        _ address: HermesAddress,
+        accepts parcelKind: HermesParcelKind
+    ) -> Bool {
+        finalAcceptances[address]?.contains(parcelKind) == true
+    }
+}
