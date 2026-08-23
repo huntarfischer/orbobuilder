456d31f772564d693505178ea724c96deab40c11
huntarfischer <huntarfischer@gmail.com>
2026-08-23 13:42:51 -0500

    RINGV2-2 prove complete 360-template atlas

--- native/OrboCore/Tests/OrboCoreTests/RingTemplateTests.swift
+++ native/OrboCore/Tests/OrboCoreTests/RingTemplateTests.swift
@@ -14,4 +14,40 @@ final class RingTemplateTests: XCTestCase {
         XCTAssertNil(RingTemplate(-1))
         XCTAssertNil(RingTemplate(360))
     }
+
+    func testRingOwnsExactlyOneTemplateForEveryDegree() throws {
+        XCTAssertEqual(Ring.templates.count, Ring.degrees)
+        XCTAssertEqual(Ring.templates.map(\.sourceDegree), Array(0..<Ring.degrees))
+        XCTAssertEqual(try XCTUnwrap(Ring.template(forDegree: 7)).sourceDegree, 7)
+        XCTAssertNil(Ring.template(forDegree: -1))
+        XCTAssertNil(Ring.template(forDegree: 360))
+    }
+
+    func testTemplateSevenCoversAll360CellsAndMarksCanonicalTargets() throws {
+        let template = try XCTUnwrap(Ring.template(forDegree: 7))
+        XCTAssertEqual(template.cells.count, Ring.degrees)
+        XCTAssertEqual(template.cells.map(\.degree), Array(0..<Ring.degrees))
+
+        XCTAssertEqual(template[7]?.mark, .conjunction)
+        XCTAssertEqual(template[37]?.mark, .semisextile)
+        XCTAssertEqual(template[337]?.mark, .semisextile)
+        XCTAssertEqual(template[97]?.mark, .square)
+        XCTAssertEqual(template[277]?.mark, .square)
+        XCTAssertEqual(template[187]?.mark, .opposition)
+        XCTAssertNil(template[98]?.mark)
+    }
+
+    func testEveryTemplateCellMatchesExistingRingGeometry() {
+        for template in Ring.templates {
+            let source = RingState(unchecked: template.sourceDegree)
+            for cell in template.cells {
+                let target = RingState(unchecked: cell.degree)
+                XCTAssertEqual(
+                    cell.mark,
+                    Ring.relation(between: source, and: target),
+                    "template \(template.sourceDegree), cell \(cell.degree)"
+                )
+            }
+        }
+    }
 }
