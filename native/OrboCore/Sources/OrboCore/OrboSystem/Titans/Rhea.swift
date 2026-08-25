4f28cb9863127dee3c8931a1109cf0e996a3a264
huntarfischer <huntarfischer@gmail.com>
2026-08-24 19:39:54 -0500

    Add Rhea testimony contract

--- native/OrboCore/Sources/OrboCore/OrboSystem/Titans/Rhea.swift
+++ native/OrboCore/Sources/OrboCore/OrboSystem/Titans/Rhea.swift
@@ -1,3 +1,12 @@
+/// Immutable testimony returned by Rhea.
+public struct RheaPass: Sendable {
+    public let field: Mater.QualifiedField
+
+    internal init(field: Mater.QualifiedField) {
+        self.field = field
+    }
+}
+
 /// Keeper of Mater.
 ///
 /// Rhea does not reimplement planetary condition. She is the authoritative
@@ -8,5 +17,12 @@ public enum Rhea {
         sect: Sect?
     ) -> Mater.QualifiedField {
         Mater.qualifyField(longitudes, sect: sect)
+    }
+
+    public static func testify(
+        _ longitudes: [Planet: CelestialLongitude],
+        sect: Sect?
+    ) -> RheaPass {
+        RheaPass(field: bear(longitudes, sect: sect))
     }
 }
