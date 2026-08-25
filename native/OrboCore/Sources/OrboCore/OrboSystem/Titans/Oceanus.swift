4229d698b861382bc18b1e8a232e708efbce8ed1
huntarfischer <huntarfischer@gmail.com>
2026-08-24 19:40:03 -0500

    Add Oceanus testimony contract

--- native/OrboCore/Sources/OrboCore/OrboSystem/Titans/Oceanus.swift
+++ native/OrboCore/Sources/OrboCore/OrboSystem/Titans/Oceanus.swift
@@ -1,3 +1,12 @@
+/// Immutable testimony returned by Oceanus.
+public struct OceanusPass: Sendable {
+    public let objectTemplates: [RingObjectTemplate]
+
+    internal init(objectTemplates: [RingObjectTemplate]) {
+        self.objectTemplates = objectTemplates
+    }
+}
+
 /// Keeper of Ring.
 ///
 /// Oceanus does not reimplement angular relation. He is the authoritative
@@ -12,5 +21,13 @@ public enum Oceanus {
         in dna: AstroDNA
     ) -> RingObjectTemplate {
         Ring.objectTemplate(for: gene, in: dna)
+    }
+
+    public static func testify(_ dna: AstroDNA) -> OceanusPass {
+        OceanusPass(
+            objectTemplates: AstroDNAGene.canonicalOrder.map { gene in
+                encircle(gene, in: dna)
+            }
+        )
     }
 }
