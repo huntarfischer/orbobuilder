945685d09c004cf2a38a956148dc5d0a43abb809
huntarfischer <huntarfischer@gmail.com>
2026-08-24 19:39:47 -0500

    Add Themis testimony contract

--- native/OrboCore/Sources/OrboCore/OrboSystem/Titans/Themis.swift
+++ native/OrboCore/Sources/OrboCore/OrboSystem/Titans/Themis.swift
@@ -1,3 +1,12 @@
+/// Immutable testimony returned by Themis.
+public struct ThemisPass: Sendable {
+    public let imprint: Tympan.Imprint
+
+    internal init(imprint: Tympan.Imprint) {
+        self.imprint = imprint
+    }
+}
+
 /// Keeper of Tympan.
 ///
 /// Themis does not reimplement whole-sign form. She is the authoritative
@@ -5,5 +14,9 @@
 public enum Themis {
     public static func set(_ risingSign: Sign) -> Tympan.Imprint {
         Tympan.imprint(for: risingSign)
+    }
+
+    public static func testify(_ risingSign: Sign) -> ThemisPass {
+        ThemisPass(imprint: set(risingSign))
     }
 }
