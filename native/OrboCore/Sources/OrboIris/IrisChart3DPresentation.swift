b3396e0a2a89ae83bc05bbd73ed00261656f56e1
huntarfischer <huntarfischer@gmail.com>
2026-08-26 16:43:14 -0500

    Add IX3 body size presentation mode

--- native/OrboCore/Sources/OrboIris/IrisChart3DPresentation.swift
+++ native/OrboCore/Sources/OrboIris/IrisChart3DPresentation.swift
@@ -11,14 +11,17 @@ public struct IrisChart3DPresentation: Hashable, Sendable {
     public let azimuthDegrees: Double
     public let inclinationDegrees: Double
     public let cameraProjection: IrisCameraProjection
+    public let bodySizeMode: IrisBodySizeMode
 
     public init(
         azimuthDegrees: Double = 20,
         inclinationDegrees: Double = 7,
-        cameraProjection: IrisCameraProjection = .orthographic
+        cameraProjection: IrisCameraProjection = .orthographic,
+        bodySizeMode: IrisBodySizeMode = .planetSized
     ) {
         self.azimuthDegrees = azimuthDegrees
         self.inclinationDegrees = inclinationDegrees
         self.cameraProjection = cameraProjection
+        self.bodySizeMode = bodySizeMode
     }
 }
