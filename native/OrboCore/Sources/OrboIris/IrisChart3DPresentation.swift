44211e781809c6c06441e8a0cf8704c1b66d3710
huntarfischer <huntarfischer@gmail.com>
2026-08-26 16:59:17 -0500

    Add IX4 track presentation controls

--- native/OrboCore/Sources/OrboIris/IrisChart3DPresentation.swift
+++ native/OrboCore/Sources/OrboIris/IrisChart3DPresentation.swift
@@ -12,16 +12,22 @@ public struct IrisChart3DPresentation: Hashable, Sendable {
     public let inclinationDegrees: Double
     public let cameraProjection: IrisCameraProjection
     public let bodySizeMode: IrisBodySizeMode
+    public let trackOrder: IrisTrackOrder
+    public let trackExpansion: Double
 
     public init(
         azimuthDegrees: Double = 20,
         inclinationDegrees: Double = 7,
         cameraProjection: IrisCameraProjection = .orthographic,
-        bodySizeMode: IrisBodySizeMode = .planetSized
+        bodySizeMode: IrisBodySizeMode = .planetSized,
+        trackOrder: IrisTrackOrder = .astroDNA,
+        trackExpansion: Double = 1.0
     ) {
         self.azimuthDegrees = azimuthDegrees
         self.inclinationDegrees = inclinationDegrees
         self.cameraProjection = cameraProjection
         self.bodySizeMode = bodySizeMode
+        self.trackOrder = trackOrder
+        self.trackExpansion = min(max(trackExpansion, 0.0), 1.0)
     }
 }
