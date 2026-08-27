c67097974d95dfa3fbdb3cf3598e4f9cee085b79
huntarfischer <huntarfischer@gmail.com>
2026-08-27 04:18:26 -0500

    Let Orbo perform the Stage 1 FOH script

--- native/OrboCore/Sources/OrboCore/OrboSystem/Orbo/Orbo.swift
+++ native/OrboCore/Sources/OrboCore/OrboSystem/Orbo/Orbo.swift
@@ -1,10 +1,35 @@
 public struct Orbo: Hashable, Sendable {
     public private(set) var frontOfHouse: OrboFrontOfHouseState
     public private(set) var backOfHouse: OrboBackOfHouseState
+    public private(set) var onboardingSession: OrboOnboardingSession?
 
     public init() {
         self.frontOfHouse = .resting
         self.backOfHouse = .idle
+        self.onboardingSession = nil
+    }
+
+    @discardableResult
+    public mutating func beginOnboarding() -> OrboOnboardingBeat {
+        if onboardingSession == nil {
+            onboardingSession = OrboOnboardingSession()
+            frontOfHouse = .onboarding
+        }
+
+        return onboardingSession!.currentBeat
+    }
+
+    @discardableResult
+    public mutating func respondToOnboarding(
+        _ response: OrboOnboardingResponse
+    ) throws -> OrboOnboardingBeat {
+        guard var session = onboardingSession else {
+            throw OrboOnboardingFailure.notStarted
+        }
+
+        try session.respond(response)
+        onboardingSession = session
+        return session.currentBeat
     }
 
     mutating func transitionFrontOfHouse(to state: OrboFrontOfHouseState) {
