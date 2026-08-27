5ef10900d779036a43c02a62083386636da61369
huntarfischer <huntarfischer@gmail.com>
2026-08-27 05:19:37 -0500

    Give Orbo pipeline fixture stable Hermes handoff time

--- native/OrboCore/Tests/OrboCoreTests/OrboSystem/Orbo/OrboPipelineFixture.swift
+++ native/OrboCore/Tests/OrboCoreTests/OrboSystem/Orbo/OrboPipelineFixture.swift
@@ -4,6 +4,7 @@ import Foundation
 enum OrboPipelineFixture {
     static let subjectID = HermesSubjectID(rawValue: "orbo.pipeline.native-001")!
     static let packageID = HermesPackageID(UUID(uuidString: "11111111-2222-3333-4444-555555555555")!)
+    static let handoffAt = AbsoluteInstant(unixSecondsSince1970: 1_777_000_000)!
     static let name = "Traveler"
     static let birthDate = CivilDate(year: 1990, month: 5, day: 17)!
     static let birthTime = CivilClockTime(hour: 14, minute: 32)!
